062fa25bc856fed27cc72f097e1ea896c6854585
Alysha Pursley <117888047+apursley2012@users.noreply.github.com>
2025-07-25 12:06:55 -0400

    Module 3: Backend and database features implemented

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,12 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json',
+'utf8'));
+
+/* Get travel view */
+const travel = (req, res) => {
+    res
+}
+
 module.exports.travel = function(req, res) {
     res.render('travel', { title: 'Travlr Getaways' });
 };
