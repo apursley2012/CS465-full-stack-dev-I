8e43d32ed7ead236b8eb6e4a81b1368b53ef0fb8
Alysha Pursley <117888047+apursley2012@users.noreply.github.com>
2025-08-08 21:40:50 -0400

    Module 5

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs = require('hbs');
-require('./app_server/models/db');
 
 
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var travelRouter = require('./app_server/routes/travel');
+var userRouter = require('./app_server/routes/user'); 
+var apiRouter = require('./app_server/routes/api');
+
+var handlebars = require('hbs');
+
+// Bring in the database connection
+require('./app_server/models/db');
 
 var app = express();
 
+// View engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/app_server/views/partials');
+
+// Register handlebars partials (https://www.nomjs.com/packages/hbs)
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,8 +33,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Wire-up routes to controllers
 app.use('/', indexRouter);
+app.use('/user', userRouter);
 app.use('/', travelRouter);
+app.use('/api', apiRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
