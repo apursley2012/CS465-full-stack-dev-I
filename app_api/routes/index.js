6bb9ad3f29cc2f03f47b407db92051c6372ad04d
Alysha Pursley <117888047+apursley2012@users.noreply.github.com>
2025-08-13 05:13:01 -0400

    Update index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,8 +1,16 @@
-const express = require('express');
+const express = require('express'); //express app
 const router = express.Router();
-const ctrlTrips = require('../controllers/trips');
 
-router.get('/trips', ctrlTrips.tripsList);
-router.get('/trips/:tripCode', ctrlTrips.tripsFindByCode);
+// where we import the controllers we will route
+const tripsController = require('../controllers/trips');
+
+// define route for trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList);
+
+router
+    .route('/trips/:tripCode') 
+    .get(tripsController.tripsFindByCode);
 
 module.exports = router;
